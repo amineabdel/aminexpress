1fba781932c31c0010fa4f03d97c48936ab10ce7
amineabdel <amine.oostende@hotmail.com>
2019-08-26 15:36:29 +0200

    basis examen

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 var request = require('request');
+var read = require('read-file');
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 
@@ -13,9 +14,13 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+}
 
 var db;
-var url = "mongodb://localhost:27017/"
+var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, (err, client) => {
     if (err) return console.log(err);
@@ -23,97 +28,14 @@ MongoClient.connect(url, (err, client) => {
 
     app.listen(3000, () => {
         console.log("poort 3000 werkt");
+
     })
+
 })
 
 
 app.get("/", (req, res) => {
-    db.collection("inhaal").find().toArray((err, result) => {
-        if (err) return console.log(err);
-        //console.log(result[0].body);
-
-        res.render("index.ejs", {
-            namen: result
-        })
-    })
-
-})
-
-app.get("/search", (req, res) => {
-    //console.log(req);
-
-    res.render("search.ejs", {
-        student: {}
+    db.collection("overtredingen").find().toArray((err, result) => {
+        res.render
     })
 })
-
-app.post("/search", (req, res) => {
-    query = {
-            naam: req.body.naam
-        }
-        //console.log(query);
-
-    db.collection("inhaal").find({
-        "body": {
-            naam: req.body.naam
-        }
-    }, (err, result) => {
-        if (err) return console.log(err);
-        // console.log(result);
-
-        res.render("search.ejs", {
-            student: result
-        })
-    })
-
-
-
-})
-
-
-app.post("/aanvraag", (req, res) => {
-    //console.log(req.body);
-    //req.body
-    query = {
-        'naam': req.body.naam,
-        'examen': req.body.examen,
-        'reden': req.body.reden
-    };
-
-    db.collection("inhaal").save({
-        _id: query,
-        body: req.body
-    }, (err, result) => {
-        if (err) return console.log(err);
-        //console.log(result);
-        //res.redirect("/");
-        res.render("search.ejs", {
-            student: result
-        })
-    })
-})
-
-
-
-
-
-// student: {
-//     'naam': req.body.naam,
-//     'examen': req.body.examen,
-//     'reden': req.body.reden,
-//     'datinex': req.body.datinex
-// }
-
-// db.collection("inhaal").updateOne({
-//     'naam': req.body.naam,
-//     'examen': req.body.examen,
-//     'reden': req.body.reden
-// }, {
-//     body: req.body
-// }, {
-//     upsert: true
-// }, (err, result) => {
-//     if (err) return console.log(err);
-//     res.redirect("/")
-
-// })
